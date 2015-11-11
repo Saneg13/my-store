ccaacddc474fd18b875d6128f878d54b9b91ddcd
Saneg <SANEG@example.com>
2015-11-11 22:33:17 +0200

    Second Commit

---
+++ htdocs/upload/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://sanegstore.byethost12.com/upload/admin/');
+define('HTTP_CATALOG', 'http://sanegstore.byethost12.com/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://sanegstore.byethost12.com/upload/admin/');
+define('HTTPS_CATALOG', 'http://sanegstore.byethost12.com/upload/');
+
+// DIR
+define('DIR_APPLICATION', '/home/vol4_5/byethost12.com/b12_16865898/htdocs/upload/admin/');
+define('DIR_SYSTEM', '/home/vol4_5/byethost12.com/b12_16865898/htdocs/upload/system/');
+define('DIR_LANGUAGE', '/home/vol4_5/byethost12.com/b12_16865898/htdocs/upload/admin/language/');
+define('DIR_TEMPLATE', '/home/vol4_5/byethost12.com/b12_16865898/htdocs/upload/admin/view/template/');
+define('DIR_CONFIG', '/home/vol4_5/byethost12.com/b12_16865898/htdocs/upload/system/config/');
+define('DIR_IMAGE', '/home/vol4_5/byethost12.com/b12_16865898/htdocs/upload/image/');
+define('DIR_CACHE', '/home/vol4_5/byethost12.com/b12_16865898/htdocs/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/vol4_5/byethost12.com/b12_16865898/htdocs/upload/system/storage/download/');
+define('DIR_LOGS', '/home/vol4_5/byethost12.com/b12_16865898/htdocs/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/vol4_5/byethost12.com/b12_16865898/htdocs/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/home/vol4_5/byethost12.com/b12_16865898/htdocs/upload/system/storage/upload/');
+define('DIR_CATALOG', '/home/vol4_5/byethost12.com/b12_16865898/htdocs/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'sql303.byethost12.com');
+define('DB_USERNAME', 'b12_16865898');
+define('DB_PASSWORD', 'ass404ryon');
+define('DB_DATABASE', 'b12_16865898_new_db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ htdocs/upload/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://sanegstore.byethost12.com/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://sanegstore.byethost12.com/upload/');
+
+// DIR
+define('DIR_APPLICATION', '/home/vol4_5/byethost12.com/b12_16865898/htdocs/upload/catalog/');
+define('DIR_SYSTEM', '/home/vol4_5/byethost12.com/b12_16865898/htdocs/upload/system/');
+define('DIR_LANGUAGE', '/home/vol4_5/byethost12.com/b12_16865898/htdocs/upload/catalog/language/');
+define('DIR_TEMPLATE', '/home/vol4_5/byethost12.com/b12_16865898/htdocs/upload/catalog/view/theme/');
+define('DIR_CONFIG', '/home/vol4_5/byethost12.com/b12_16865898/htdocs/upload/system/config/');
+define('DIR_IMAGE', '/home/vol4_5/byethost12.com/b12_16865898/htdocs/upload/image/');
+define('DIR_CACHE', '/home/vol4_5/byethost12.com/b12_16865898/htdocs/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/vol4_5/byethost12.com/b12_16865898/htdocs/upload/system/storage/download/');
+define('DIR_LOGS', '/home/vol4_5/byethost12.com/b12_16865898/htdocs/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/vol4_5/byethost12.com/b12_16865898/htdocs/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/home/vol4_5/byethost12.com/b12_16865898/htdocs/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'sql303.byethost12.com');
+define('DB_USERNAME', 'b12_16865898');
+define('DB_PASSWORD', 'ass404ryon');
+define('DB_DATABASE', 'b12_16865898_new_db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
